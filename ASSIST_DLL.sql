8e0cf9cb59712b4431f7f422d369d31ea240046d
Arthur Silvestre de Brito Viana <art.brito@outlook.com>
2019-02-20 13:36:15 -0300

    Add files via upload
    João

---
+++ ASSIST_DLL.sql
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------------------------------
+                                Inserção AON AUTOPROD
+----------------------------------------------------------------------------------------*/
+
+INSERT INTO USUARIO_AUTOPROD.OPCAO_DADO(ID,GRUPO_OPCAO_ID,DS_OPCAO_DADO)VALUES(2000390,861,'AON Assist');
+INSERT INTO USUARIO_AUTOPROD.OPCAO_DADO(ID,GRUPO_OPCAO_ID,DS_OPCAO_DADO)VALUES(2000391,2000015,'AON Assist');
+INSERT INTO USUARIO_AUTOPROD.OPCAO_DADO(ID,GRUPO_OPCAO_ID,DS_OPCAO_DADO)VALUES(2000392,2000016,'AON Assist');
+
+
+
+/*--------------------------------------------------------------------------------------
+                                         FIM
+----------------------------------------------------------------------------------------*/
